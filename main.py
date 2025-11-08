1cc77d538981293645e6898afc614d0570297087
twtest <tw@test.com>
2025-11-07 23:32:15 -0500

    Add rotation to player sprite

--- main.py
+++ main.py
@@ -17,12 +17,15 @@ def main():
 
     while(True):
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
 
         player.draw(screen)
+        player.update(dt)
 
         pygame.display.flip()
 
