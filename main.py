0e2373845160e80d561e9898dd29e9c14f539cf1
twtest <tw@test.com>
2025-11-07 23:24:58 -0500

    Add clock and player sprite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -10,14 +11,22 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while(True):
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
 
+    while(True):
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        player.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
